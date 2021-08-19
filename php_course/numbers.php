00861936f0f99fd561d352a6cfb0d2387e8fda08
Johnathan Nguyen <haungocvo@gmail.com>
2021-08-18 21:08:19 -0700

    numbers

---
+++ php_course/numbers.php
@@ -0,0 +1,29 @@
+<?php
+
+$a = 5;
+$b = 4;
+$c = 1.2;
+
+// Arithmetic operations\
+echo "<br>";
+echo $a + $b * $c . "<br>";
+
+// This gets the remainder
+echo $a % $b . "<br>";
+
+// Assignment with math operators
+$a = $a + $b;
+echo $a . "<br>";
+
+// $a = $a - $b.'<br>';
+echo $a . "<br>";
+
+// Increment operator
+echo $a++ . "<br>";
+echo ++$a;
+
+// Decrement operator
+echo $a-- . "<br>";
+echo --$a . "<br>";
+
+?>
